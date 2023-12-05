906f2a376874c100f7aceb3c7225db20d06ff190
Andrea Maggi <andrea@maggicontrols.com>
2023-12-05 17:55:45 +0100

    Released version 0.2.0 depending on FS version 2.4.16

--- fs/imapfs/__init__.py
+++ fs/imapfs/__init__.py
@@ -1,8 +1,8 @@
 # coding: utf-8
 """Pyfilesystem2 over IMAP using IMAPClient.
 """
-from __future__ import absolute_import
-from __future__ import unicode_literals
+
+__import__("pkg_resources").declare_namespace(__name__)  # type: ignore
 
 from .imapfs import IMAPFS
 from .imapfs import Info
@@ -12,22 +12,4 @@ __all__ = ['IMAPFS', 'Info']
 __license__ = "MIT"
 __copyright__ = "Copyright (c) 2017-2019 Andrea Maggi"
 __author__ = "Andrea Maggi <andrea@maggicontrols.com>"
-__version__ = 'dev'
-
-# import fs
-# import os
-# 
-# result_path = os.path.realpath(
-#     os.path.join(__file__, '..', '..', 'fs'))
-# 
-# print(result_path)
-# fs.__path__.insert(0, result_path)
-
-# Dynamically get the version of the installed module
-try:
-    import pkg_resources
-    __version__ = pkg_resources.get_distribution(__name__).version
-except Exception: # pragma: no cover
-    pkg_resources = None
-finally:
-    del pkg_resources
+__version__ = '0.2.0'
